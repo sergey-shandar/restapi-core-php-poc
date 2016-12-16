217116826f89cae4bd549de35dfa7549b0a1c3d4
Sergey Shandar <seshanda@microsoft.com>
2016-12-16 14:29:40 -0800

    Core and Test templates.

---
+++ src/Core.php
@@ -0,0 +1,22 @@
+<?php
+namespace RestApiCore;
+
+
+class Core
+{
+    /**
+     * @param mixed $data
+     */
+    public static function serialize($data)
+    {
+        $typeName = gettype($data);
+        switch ($typeName)
+        {
+            case 'boolean':
+                return null;
+            case 'integer':
+                return null;
+            case
+        }
+    }
+}

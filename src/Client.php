2a99a4b21562582dd861564a56f9f453e5c231ae
Sergey Shandar <seshanda@microsoft.com>
2017-01-04 17:01:13 -0800

    Return null.

--- src/Client.php
+++ src/Client.php
@@ -11,8 +11,11 @@ class Client
      * @param array $queryParameters
      * @param array $headerParameters
      * @param $body
+     *
+     * @return null
      */
     function request($path, $method, array $pathParameters, array $queryParameters, array $headerParameters, $body)
     {
+        return null;
     }
 }

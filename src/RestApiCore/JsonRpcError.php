45eebc9898808c8892692c1432f738b05bb8dad5
Sergey Shandar <seshanda@microsoft.com>
2017-02-28 14:04:27 -0800

    JSON RPC Error.

---
+++ src/RestApiCore/JsonRpcError.php
@@ -0,0 +1,31 @@
+<?php
+namespace RestApiCore;
+
+final class JsonRpcError extends \Exception
+{
+    const PARSE_ERROR = -32700;
+    const INVALID_REQUEST = -32600;
+    const METHOD_NOT_FOUND = -32601;
+    const INVALID_PARAMS = -32602;
+    const INTERNAL_ERROR = -32603;
+
+    const SERVER_ERROR_FIRST = -32000;
+
+    /**
+     * @var mixed
+     */
+    public $data;
+
+    /**
+     * JsonRpcError constructor.
+     *
+     * @param string $message
+     * @param int $code
+     * @param mixed|null $data
+     */
+    public function __construct($message, $code, $data = null)
+    {
+        parent::__construct($message, $code);
+        $this->data = $data;
+    }
+}
---
+++ test/JsonRpcErrorTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use RestApiCore\JsonRpcError;
+
+class JsonRpcErrorTest extends TestCase
+{
+    public function testConstructor()
+    {
+        try {
+            throw new JsonRpcError("Unknown method", JsonRpcError::METHOD_NOT_FOUND);
+        } catch (JsonRpcError $exception) {
+            $this->assertInstanceOf(JsonRpcError::class, $exception);
+            $this->assertSame(-32601, $exception->getCode());
+            $this->assertSame("Unknown method", $exception->getMessage());
+        }
+    }
+}
